4db1674bf81db99ff6daab3679f28386fc4248c4
Gleb <glebparkhomenko1@gmail.com>
2025-07-08 20:44:43 +0300

    added check for user blockStatus.

--- src/main/java/com/epam/finaltask/config/component/CustomAuthenticationProvider.java
+++ src/main/java/com/epam/finaltask/config/component/CustomAuthenticationProvider.java
@@ -2,6 +2,8 @@ package com.epam.finaltask.config.component;
 
 import com.epam.finaltask.service.config.CustomUserDetailsService;
 import com.epam.finaltask.service.config.LoginAttemptService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -13,6 +15,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    Logger logger = LoggerFactory.getLogger(CustomAuthenticationProvider.class);
 
     private final CustomUserDetailsService userDetailsService;
     private final LoginAttemptService loginAttemptService;
@@ -31,13 +35,19 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
         String username = authentication.getName();
 
         if (loginAttemptService.isBlocked(username)) {
+            logger.info("User account is locked due to too many failed login attempts");
             throw new LockedException("User account is locked due to too many failed login attempts");
         }
 
         UserDetails userDetails = userDetailsService.loadUserByUsername(username);
         String presentedPassword = authentication.getCredentials().toString();
 
+        if (!userDetails.isEnabled()) {
+            logger.info("User account is blocked");
+            throw new LockedException("User account is blocked");
+        }
         if (!passwordEncoder.matches(presentedPassword, userDetails.getPassword())) {
+            logger.info("Invalid credentials");
             loginAttemptService.loginFailed(username);
             throw new BadCredentialsException("Invalid credentials");
         }
